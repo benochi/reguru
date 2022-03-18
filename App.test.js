e9d24b8ef8b3d213b97cebc4ed0cb8c87da62f75
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-03-18 10:43:40 -0500

    Add files via upload

---
+++ App.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+test('renders', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+});
+
